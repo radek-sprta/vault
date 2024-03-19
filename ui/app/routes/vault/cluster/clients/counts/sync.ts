dd5053bcfede617e0dded63a20ea6fe71eb26d7f
Noelle Daley <noelledaley@users.noreply.github.com>
2024-03-18 21:41:59 -0700

    routes/clients/counts/sync: fetch activated features

--- ui/app/routes/vault/cluster/clients/counts/sync.ts
+++ ui/app/routes/vault/cluster/clients/counts/sync.ts
@@ -4,5 +4,36 @@
  */
 
 import Route from '@ember/routing/route';
+import { service } from '@ember/service';
+import { DEBUG } from '@glimmer/env';
 
-export default class ClientsCountsSyncRoute extends Route {}
+import type StoreService from 'vault/services/store';
+
+interface ActivationFlagsResponse {
+  data: {
+    activated: Array<string>;
+    unactivated: Array<string>;
+  };
+}
+
+export default class ClientsCountsSyncRoute extends Route {
+  @service declare readonly store: StoreService;
+
+  async getActivatedFeatures() {
+    try {
+      const resp: ActivationFlagsResponse = await this.store
+        .adapterFor('application')
+        .ajax('/v1/sys/activation-flags', 'GET');
+      return resp.data?.activated;
+    } catch (error) {
+      if (DEBUG) console.error(error); // eslint-disable-line no-console
+      return [];
+    }
+  }
+
+  async model() {
+    return {
+      activatedFeatures: await this.getActivatedFeatures(),
+    };
+  }
+}
